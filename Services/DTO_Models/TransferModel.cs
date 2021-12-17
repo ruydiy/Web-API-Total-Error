50549bf6a0f371b23d938741c6b068b87a5ed177
ruydiy <radoslavtcankov@abv.bg>
2021-12-17 21:18:11 +0200

    read from file

--- Services/DTO_Models/TransferModel.cs
+++ Services/DTO_Models/TransferModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CsvHelper.Configuration.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,46 @@ namespace Services.DTO_Models
 {
     public class TransferModel
     {
+        [Name("Region")]
+        public string Region { get; set; }
 
+        [Name("Country")]
+        public string Country { get; set; }
+
+        [Name("Item Type")]
+        public string ItemType { get; set; }
+
+        [Name("Sales Channel")]
+        public string SalesChannel { get; set; }
+
+        [Name("Order Priority")]
+        public string OrderPrioriy { get; set; }
+
+        [Name("Order Date")]
+        public DateTime OrderDate { get; set; }
+
+        [Name("Order ID")]
+        public int OrderId { get; set; }
+
+        [Name("Ship Date")]
+        public DateTime ShipDate { get; set; }
+
+        [Name("Units Sold")]
+        public int UnitsSold { get; set; }
+
+        [Name("Unit Price")]
+        public decimal UnitPrice { get; set; }
+
+        [Name("Unit Cost")]
+        public decimal UnitCost { get; set; }
+
+        [Name("Total Revenue")]
+        public decimal TotalRevenue { get; set; }
+
+        [Name("Total Cost")]
+        public decimal TotalCost { get; set; }
+
+        [Name("Total Profit")]
+        public decimal TotalProfit { get; set; }
     }
 }
